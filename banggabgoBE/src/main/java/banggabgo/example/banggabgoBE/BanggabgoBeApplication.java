01dd4b38cf7eb6718239f6c6d35686fdb9e456c9
sjdlr <sjdlrnen0507@naver.com>
2023-05-31 14:18:59 +0900

    Initial commit

---
+++ banggabgoBE/src/main/java/banggabgo/example/banggabgoBE/BanggabgoBeApplication.java
@@ -0,0 +1,13 @@
+package banggabgo.example.banggabgoBE;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BanggabgoBeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BanggabgoBeApplication.class, args);
+	}
+
+}
---
+++ banggabgoBE/src/test/java/banggabgo/example/banggabgoBE/BanggabgoBeApplicationTests.java
@@ -0,0 +1,13 @@
+package banggabgo.example.banggabgoBE;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BanggabgoBeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
